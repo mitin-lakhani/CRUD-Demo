d7fb78916346c81c49cea2fed639536b3e6bd2d2
Mitin Lakhani <mitinlakhani2004@gmail.com>
2026-01-23 10:25:42 +0530

    Change Product Page UI

--- src/pages/user/Products.tsx
+++ src/pages/user/Products.tsx
@@ -1,131 +1,192 @@
-const Product = () => {
+function Products() {
     return (
-        <div className="bg-indigo-400 h-full w-full">
-            <div className="grid grid-cols-1 sm[578px]:grid-cols-2 md:grid-cols-5 p-3 gap-3">
-                <div className="bg-linear-to-r from-blue-500 to-cyan-400 w-50 h-60 rounded-2xl hover:shadow-md transition duration-700 ease-in-out shadow-black ">
-                    <img src="src/assets/product-1.jpg" className="h-1/2 w-full object-cover rounded-t-lg" alt="" />
-                    <div className="mt-3 flex gap-3 ml-2">
-                        <strong>Price</strong>
-                        <span className="text-white font-bold">$300</span>
+        <div className="grid grid-cols-4">
+            <div className="w-80 border rounded-2xl overflow-hidden shadow-sm hover:shadow-md transition">
 
-                    </div>
-                    <div>
-                        <p className="ml-2">Title</p>
-                    </div>
-                    <button className="cursor-pointer border p-1 mt-3 rounded-md text-white font-bold ml-2">BuyNow</button>
-                </div>
-                 <div className="bg-linear-to-r from-blue-500 to-cyan-400 w-50 h-60 rounded-2xl hover:shadow-md transition duration-700 ease-in-out shadow-black ">
-                    <img src="src/assets/product-1.jpg" className="h-1/2 w-full object-cover rounded-t-lg" alt="" />
-                    <div className="mt-3 flex gap-3 ml-2">
-                        <strong>Price</strong>
-                        <span className="text-white font-bold">$300</span>
+                {/* Image */}
+                <img
+                    src="src/assets/product-1.jpg"
+                    className="w-full h-48 object-cover"
+                    alt="product"
+                />
 
-                    </div>
-                    <div>
-                        <p className="ml-2">Title</p>
-                    </div>
-                    <button className="cursor-pointer border p-1 mt-3 rounded-md text-white font-bold ml-2">BuyNow</button>
-                </div>  
+                {/* Content */}
+                <div className="p-4 space-y-3">
 
-                <div className="bg-linear-to-r from-blue-500 to-cyan-400 w-50 h-60 rounded-2xl hover:shadow-md transition duration-700 ease-in-out shadow-black ">
-                    <img src="src/assets/product-1.jpg" className="h-1/2 w-full object-cover rounded-t-lg" alt="" />
-                    <div className="mt-3 flex gap-3 ml-2">
-                        <strong>Price</strong>
-                        <span className="text-white font-bold">$300</span>
+                    {/* Title */}
+                    <h3 className="font-semibold text-lg text-white">
+                        This Product
+                    </h3>
 
-                    </div>
-                    <div>
-                        <p className="ml-2">Title</p>
-                    </div>
-                    <button className="cursor-pointer border p-1 mt-3 rounded-md text-white font-bold ml-2">BuyNow</button>
-                </div>
-                <div className="bg-linear-to-r from-blue-500 to-cyan-400 w-50 h-60 rounded-2xl hover:shadow-md transition duration-700 ease-in-out shadow-black ">
-                    <img src="src/assets/product-1.jpg" className="h-1/2 w-full object-cover rounded-t-lg" alt="" />
-                    <div className="mt-3 flex gap-3 ml-2">
-                        <strong>Price</strong>
-                        <span className="text-white font-bold">$300</span>
+                    {/* Price & Category */}
+                    <div className="flex justify-between items-center">
+                        <span className="text-lg font-bold text-green-600">
+                            $300
+                        </span>
 
+                        <select className="border rounded-md px-2 py-1 text-sm focus:outline-none focus:ring-2 focus:ring-blue-400">
+                            <option className="text-black">All Categories</option>
+                            <option className="text-black">Electronics</option>
+                            <option className="text-black">Clothing</option>
+                            <option className="text-black">Furniture</option>
+                        </select>
                     </div>
-                    <div>
-                        <p className="ml-2">Title</p>
-                    </div>
-                    <button className="cursor-pointer border p-1 mt-3 rounded-md text-white font-bold ml-2">BuyNow</button>
-                </div>
-                <div className="bg-linear-to-r from-blue-500 to-cyan-400 w-50 h-60 rounded-2xl hover:shadow-md transition duration-700 ease-in-out shadow-black ">
-                    <img src="src/assets/product-1.jpg" className="h-1/2 w-full object-cover rounded-t-lg" alt="" />
-                    <div className="mt-3 flex gap-3 ml-2">
-                        <strong>Price</strong>
-                        <span className="text-white font-bold">$300</span>
 
-                    </div>
-                    <div>
-                        <p className="ml-2">Title</p>
-                    </div>
-                    <button className="cursor-pointer border p-1 mt-3 rounded-md text-white font-bold ml-2">BuyNow</button>
-                </div>
-                <div className="bg-linear-to-r from-blue-500 to-cyan-400 w-50 h-60 rounded-2xl hover:shadow-md transition duration-700 ease-in-out shadow-black ">
-                    <img src="src/assets/product-1.jpg" className="h-1/2 w-full object-cover rounded-t-lg" alt="" />
-                    <div className="mt-3 flex gap-3 ml-2">
-                        <strong>Price</strong>
-                        <span className="text-white font-bold">$300</span>
+                    {/* Divider */}
+                    <div className="h-px bg-gray-200" />
 
+                    {/* Available Quantity */}
+                    <div className="flex justify-between items-center text-sm">
+                        <span className="font-medium text-neutral-500">
+                            Available Qty
+                        </span>
+                        <span className="font-bold text-blue-600 text-lg">
+                            25
+                        </span>
                     </div>
-                    <div>
-                        <p className="ml-2">Title</p>
-                    </div>
-                    <button className="cursor-pointer border p-1 mt-3 rounded-md text-white font-bold ml-2">BuyNow</button>
-                </div>
-                <div className="bg-linear-to-r from-blue-500 to-cyan-400 w-50 h-60 rounded-2xl hover:shadow-md transition duration-700 ease-in-out shadow-black ">
-                    <img src="src/assets/product-1.jpg" className="h-1/2 w-full object-cover rounded-t-lg" alt="" />
-                    <div className="mt-3 flex gap-3 ml-2">
-                        <strong>Price</strong>
-                        <span className="text-white font-bold">$300</span>
 
-                    </div>
-                    <div>
-                        <p className="ml-2">Title</p>
-                    </div>
-                    <button className="cursor-pointer border p-1 mt-3 rounded-md text-white font-bold ml-2">BuyNow</button>
-                </div>
-                
-                <div className="bg-linear-to-r from-blue-500 to-cyan-400 w-50 h-60 rounded-2xl hover:shadow-md transition duration-700 ease-in-out shadow-black ">
-                    <img src="src/assets/product-1.jpg" className="h-1/2 w-full object-cover rounded-t-lg" alt="" />
-                    <div className="mt-3 flex gap-3 ml-2">
-                        <strong>Price</strong>
-                        <span className="text-white font-bold">$300</span>
-
-                    </div>
-                    <div>
-                        <p className="ml-2">Title</p>
-                    </div>
-                    <button className="cursor-pointer border p-1 mt-3 rounded-md text-white font-bold ml-2">BuyNow</button>
-                </div>
-                <div className="bg-linear-to-r from-blue-500 to-cyan-400 w-50 h-60 rounded-2xl hover:shadow-md transition duration-700 ease-in-out shadow-black ">
-                    <img src="src/assets/product-1.jpg" className="h-1/2 w-full object-cover rounded-t-lg" alt="" />
-                    <div className="mt-3 flex gap-3 ml-2">
-                        <strong>Price</strong>
-                        <span className="text-white font-bold">$300</span>
-
-                    </div>
-                    <div>
-                        <p className="ml-2">Title</p>
-                    </div>
-                    <button className="cursor-pointer border p-1 mt-3 rounded-md text-white font-bold ml-2">BuyNow</button>
-                </div>
-                <div className="bg-linear-to-r from-blue-500 to-cyan-400 w-50 h-60 rounded-2xl hover:shadow-md transition duration-700 ease-in-out shadow-black ">
-                    <img src="src/assets/product-1.jpg" className="h-1/2 w-full object-cover rounded-t-lg" alt="" />
-                    <div className="mt-3 flex gap-3 ml-2">
-                        <strong>Price</strong>
-                        <span className="text-white font-bold">$300</span>
-
-                    </div>
-                    <div>
-                        <p className="ml-2">Title</p>
-                    </div>
-                    <button className="cursor-pointer border p-1 mt-3 rounded-md text-white font-bold ml-2">BuyNow</button>
                 </div>
             </div>
+            <div className="w-80 border rounded-2xl overflow-hidden shadow-sm hover:shadow-md transition">
+
+                {/* Image */}
+                <img
+                    src="src/assets/product-1.jpg"
+                    className="w-full h-48 object-cover"
+                    alt="product"
+                />
+
+                {/* Content */}
+                <div className="p-4 space-y-3">
+
+                    {/* Title */}
+                    <h3 className="font-semibold text-lg text-white">
+                        This Product
+                    </h3>
+
+                    {/* Price & Category */}
+                    <div className="flex justify-between items-center">
+                        <span className="text-lg font-bold text-green-600">
+                            $300
+                        </span>
+
+                        <select className="border rounded-md px-2 py-1 text-sm focus:outline-none focus:ring-2 focus:ring-blue-400">
+                            <option className="text-black">All Categories</option>
+                            <option className="text-black">Electronics</option>
+                            <option className="text-black">Clothing</option>
+                            <option className="text-black">Furniture</option>
+                        </select>
+                    </div>
+
+                    {/* Divider */}
+                    <div className="h-px bg-gray-200" />
+
+                    {/* Available Quantity */}
+                    <div className="flex justify-between items-center text-sm">
+                        <span className="font-medium text-neutral-500">
+                            Available Qty
+                        </span>
+                        <span className="font-bold text-blue-600 text-lg">
+                            25
+                        </span>
+                    </div>
+
+                </div>
+            </div>
+            <div className="w-80 border rounded-2xl overflow-hidden shadow-sm hover:shadow-md transition">
+
+                {/* Image */}
+                <img
+                    src="src/assets/product-1.jpg"
+                    className="w-full h-48 object-cover"
+                    alt="product"
+                />
+
+                {/* Content */}
+                <div className="p-4 space-y-3">
+
+                    {/* Title */}
+                    <h3 className="font-semibold text-lg text-white">
+                        This Product
+                    </h3>
+
+                    {/* Price & Category */}
+                    <div className="flex justify-between items-center">
+                        <span className="text-lg font-bold text-green-600">
+                            $300
+                        </span>
+
+                        <select className="border rounded-md px-2 py-1 text-sm focus:outline-none focus:ring-2 focus:ring-blue-400">
+                            <option className="text-black">All Categories</option>
+                            <option className="text-black">Electronics</option>
+                            <option className="text-black">Clothing</option>
+                            <option className="text-black">Furniture</option>
+                        </select>
+                    </div>
+
+                    {/* Divider */}
+                    <div className="h-px bg-gray-200" />
+
+                    {/* Available Quantity */}
+                    <div className="flex justify-between items-center text-sm">
+                        <span className="font-medium text-neutral-500">
+                            Available Qty
+                        </span>
+                        <span className="font-bold text-blue-600 text-lg">
+                            25
+                        </span>
+                    </div>
+
+                </div>
+            </div>
+            <div className="w-80 border rounded-2xl overflow-hidden shadow-sm hover:shadow-md transition">
+
+                {/* Image */}
+                <img
+                    src="src/assets/product-1.jpg"
+                    className="w-full h-48 object-cover"
+                    alt="product"
+                />
+
+                {/* Content */}
+                <div className="p-4 space-y-3">
+
+                    {/* Title */}
+                    <h3 className="font-semibold text-lg text-white">
+                        This Product
+                    </h3>
+
+                    {/* Price & Category */}
+                    <div className="flex justify-between items-center">
+                        <span className="text-lg font-bold text-green-600">
+                            $300
+                        </span>
+
+                        <select className="border rounded-md px-2 py-1 text-sm focus:outline-none focus:ring-2 focus:ring-blue-400">
+                            <option className="text-black">All Categories</option>
+                            <option className="text-black">Electronics</option>
+                            <option className="text-black">Clothing</option>
+                            <option className="text-black">Furniture</option>
+                        </select>
+                    </div>
+
+                    {/* Divider */}
+                    <div className="h-px bg-gray-200" />
+
+                    {/* Available Quantity */}
+                    <div className="flex justify-between items-center text-sm">
+                        <span className="font-medium text-neutral-500">
+                            Available Qty
+                        </span>
+                        <span className="font-bold text-blue-600 text-lg">
+                            25
+                        </span>
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     )
 }
-export default Product;
+export default Products;
