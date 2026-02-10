91a8a68a0f0e61b217e12b5e7361e3d6a5fe260e
Mitin Lakhani <mitinlakhani2004@gmail.com>
2026-02-10 13:15:35 +0530

    add otp system

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -4,8 +4,8 @@ export interface IUser {
   name: string;
   email: string;
   password: string;
-  otp:number;
-  status:string;
+    otp?:number;
+    status?:string;
   images:string;
 }
 export interface UserTableProps {
