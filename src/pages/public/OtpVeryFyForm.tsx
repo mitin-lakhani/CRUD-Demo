101bf4a87415b4b8c8533403562d1f11eadf2111
Mitin Lakhani <mitinlakhani2004@gmail.com>
2026-03-09 15:51:35 +0530

    update change

--- src/pages/public/OtpVeryFyForm.tsx
+++ src/pages/public/OtpVeryFyForm.tsx
@@ -28,7 +28,7 @@ const OtpVerifyForm = ({ email }: { email: string }) => {
     try {
 
       setIsVerifyOtp(true);
-      const response = await axios.post("https://backendapi-mo9g.onrender.com/api/auth/", {
+      const response = await axios.post("https://backendapi-mo9g.onrender.com/api/auth/verify-otp", {
         email: email,
         otp: data.otp
 
